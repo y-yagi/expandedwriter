67ae75e871287db1e415499b8e491ef2aa4eea52
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2023-05-02 07:47:43 +0900

    First commit

---
+++ expandedwriter.go
@@ -0,0 +1,65 @@
+package expandedwriter
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+type Expandedwriter struct {
+	w              io.Writer
+	fields         []string
+	values         [][]string
+	valueMaxLength int
+	fieldMaxLength int
+}
+
+func NewWriter(w io.Writer) *Expandedwriter {
+	return &Expandedwriter{w: w}
+}
+
+func (ew *Expandedwriter) SetFields(fields []string) {
+	for _, v := range fields {
+		if ew.fieldMaxLength < len(v) {
+			ew.fieldMaxLength = len(v)
+		}
+	}
+	ew.fields = fields
+}
+
+func (ew *Expandedwriter) Append(value []string) {
+	for _, v := range value {
+		if ew.valueMaxLength < len(v) {
+			ew.valueMaxLength = len(v)
+		}
+	}
+	ew.values = append(ew.values, value)
+}
+
+func (ew *Expandedwriter) Render() error {
+	result := ""
+	delimiterSizeForFieldAndValue := 3
+
+	for i, value := range ew.values {
+		header := fmt.Sprintf("--[ Data %d ]", i+1)
+		if len(header) < ew.valueMaxLength+ew.fieldMaxLength+delimiterSizeForFieldAndValue {
+			header += strings.Repeat("-", ew.valueMaxLength+ew.fieldMaxLength+delimiterSizeForFieldAndValue-len(header))
+		}
+
+		result += header
+
+		for i, v := range value {
+			field := ""
+			if len(ew.fields) > i {
+				field = ew.fields[i]
+			}
+
+			result += fmt.Sprintf("\n%-*s | %s", ew.fieldMaxLength, field, v)
+		}
+		result += "\n"
+	}
+
+	ew.w.Write([]byte(result))
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/y-yagi/expandedwriter
+
+go 1.20
